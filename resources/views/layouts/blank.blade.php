e1b60272bb05de2526479c70ccfc9d0f6e5ea2f6
Nia Ramadhani .R <ramdhanin317@gmail.com>
2026-06-22 12:31:39 +0800

    Pertemuan 11: layouting

---
+++ resources/views/layouts/blank.blade.php
@@ -0,0 +1,16 @@
+<x-app>
+
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <div class="card shadow p-3">
+        <h5 class="fw-bold mb-0">{{ $title }}</h5>
+    </div>
+
+    @push('modals')
+    @endpush
+
+    @push('scripts')
+    @endpush
+
+
+</x-app>
